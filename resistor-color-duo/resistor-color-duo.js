63b60404e2ccbfd7cc4ef59f582a9418d45ee6f2
Pablo Galisteo <pablogalisteo86@gmail.com>
2021-03-26 21:55:18 +0100

    new solution

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -4,23 +4,18 @@
 //
 
 export const decodedValue = ([color1, color2]) => {
-
-  const indexColor1 = colors.indexOf(color1)
-  const indexColor2 = colors.indexOf(color2)
-
-  return Number(`${indexColor1}${indexColor2}`)
-
+  return parseInt(`${colors.indexOf(color1)}${colors.indexOf(color2)}`)
 };
 
 const colors = [
-  'Black',
-  'Brown',
-  'Red',
-  'Orange',
-  'Yellow',
-  'Green',
-  'Blue',
-  'Violet',
-  'Grey',
-  'White'
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white'
 ]
